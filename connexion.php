483de3a57c5ba2dad6062bda9679ec1282d9057e
kchaou-mariem <kchaoumariem63@gmail.com>
2025-11-04 22:56:10 +0100

    Initial commit: first version of application_covoiturage

---
+++ connexion.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";   
+$username = "root";          
+$password = "";              
+$dbname = "covoiturage"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Échec de la connexion : " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8");
+
+?>
